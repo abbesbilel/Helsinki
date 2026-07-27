3811e22ed05455b3b51c818c4310be5494e9e1e1
abbesbilel <b_abbes@estin.dz>
2026-07-27 20:58:54 +0100

    finished exercice 14 of phonebook

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,41 +1,53 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import phonebookService from './services/phonebook'
 
 
 
-  const PersonLine = ({ person }) => (
-    <li>{person.name} {person.number}</li>
-  )
 
-  const People = ({ persons }) => (
-    <ul>
-      {persons.map(person => (
-        <PersonLine key={person.name} person={person} />
-      ))}
-    </ul>
-  )
 
-  const Form = ({newName, newNumber, handleChange, handleNumberChange, addPerson}) => (
-    <form onSubmit={addPerson}>
-      <div>
-        name: <input value={newName} onChange={handleChange} />
-      </div>
-      <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  )
+const PersonLine = ({ person, removePerson }) => (
+  <li>{person.name} {person.number}
+      <button onClick={removePerson}>delete</button>
+  </li>
+)
+
+const People = ({ persons, removePerson}) => (
+  <ul>
+    {persons.map(person => (
+      <PersonLine key={person.name} person={person} removePerson={() => removePerson(person.id)} />
+    ))}
+  </ul>
+)
+
+const Form = ({ newName, newNumber, handleChange, handleNumberChange, addPerson }) => (
+  <form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handleChange} />
+    </div>
+    <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
 
 const App = () => {
-    const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([])
 
-  useEffect(()=> {
-    axios.get("http://localhost:3001/persons")
-    .then(response =>
-      setPersons(response.data)
-    )
-  },[])
+    const removePerson = id => {
+    const personToRemove = `http://localhost:3001/persons/${id}`
+    phonebookService.remove(personToRemove).then(response => {
+      setPersons(persons.filter(p => p.id !== id))
+    })
+  }
+
+  useEffect(() => {
+    phonebookService.getAll()
+      .then(response =>
+        setPersons(response)
+      )
+  }, [])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
@@ -48,9 +60,12 @@ const App = () => {
       return
     }
     const personObject = { name: newName, number: newNumber }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    phonebookService.create(personObject)
+      .then((response => {
+        setPersons(persons.concat(response))
+        setNewName('')
+        setNewNumber('')
+      }))
   }
 
   const handleChange = (event) => {
@@ -65,28 +80,28 @@ const App = () => {
     setNewSearch(event.target.value)
   }
 
-const personsToShow = persons.filter(person => 
+  const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(newSearch.toLowerCase())
   )
 
-return (
-  <div>
-    <h2>Phonebook</h2>
+  return (
     <div>
-      filter shown with <input value={newSearch} onChange={handleSearchChange} />
-    </div>
-    <ul>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newSearch} onChange={handleSearchChange} />
+      </div>
+      <ul>
         {personsToShow.map(person => (
           <PersonLine key={person.name} person={person} />
         ))}
       </ul>
-    <h2>Add a new</h2>
-    <Form newName={newName} newNumber={newNumber} handleChange={handleChange} handleNumberChange={handleNumberChange} addPerson={addPerson} />
-    <h2>Numbers</h2>
-    <People persons={persons} />
+      <h2>Add a new</h2>
+      <Form newName={newName} newNumber={newNumber} handleChange={handleChange} handleNumberChange={handleNumberChange} addPerson={addPerson} />
+      <h2>Numbers</h2>
+      <People persons={persons} removePerson={removePerson} />
 
-  </div>
-)
+    </div>
+  )
 }
 
 export default App
