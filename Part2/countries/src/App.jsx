2497cc640fbaae6fc5435bd34a406f6c2a28219a
abbesbilel <b_abbes@estin.dz>
2026-07-30 22:12:39 +0100

    finished part2 exercices

--- Part2/countries/src/App.jsx
+++ Part2/countries/src/App.jsx
@@ -2,8 +2,27 @@ import { useState, useEffect } from 'react'
 import axios from "axios";
 import countriesServices from './services/countries'
 
-const CountryDetail = ({country}) => {
+const CountryDetail = ({ country }) => {
   const languages = Object.values(country.languages || {})
+  const api_key = import.meta.env.VITE_SOME_KEY
+  const [weather, setWeather] = useState(null)
+  const capital = country.capital ? country.capital[0] : null
+
+  useEffect(() => {
+    if (!capital) return
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+      )
+      .then(response => {
+        setWeather(response.data)
+      })
+      .catch(error => {
+        console.error('Error fetching weather:', error)
+      })
+  }, [capital, api_key])
+  console.log(weather)
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -15,27 +34,49 @@ const CountryDetail = ({country}) => {
           <li key={language}>{language}</li>
         ))}
       </ul>
-      <img src={country.flags.png}/>
+      <img src={country.flags.png} />
+      {weather && (
+        <div>
+          <h2>Weather in {capital}</h2>
+          <p>temperature {weather.main.temp} Celsius</p>
+          
+          {weather.weather[0] && (
+            <img 
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+              alt={weather.weather[0].description} 
+            />
+          )}
+
+          <p>wind {weather.wind.speed} m/s</p>
+        </div>
+      )}
+
     </div>
   )
 }
 
-const Display = ({ countriesToShow }) => {
-  if (countriesToShow.length === 1) {
-    return <CountryDetail country={countriesToShow[0]} />
-  }
-  else if (countriesToShow.length <= 10) {
-    return (
-      <ul>
-        {countriesToShow.map(country => (
-          <li key={country.name.common}>{country.name.common}</li>
-        ))}
-      </ul>
-    )
-  }
-  else if (countriesToShow.length > 10) {
-    return <p>too many matches, specify another filter</p>
-  }
+const Display = ({ countriesToShow, selectedCountry, onShowCountry }) => {
+  if (selectedCountry) {
+    return <CountryDetail country={selectedCountry} />
+  } else
+    if (countriesToShow.length === 1) {
+      return <CountryDetail country={countriesToShow[0]} />
+    }
+    else if (countriesToShow.length <= 10) {
+      return (
+        <ul>
+          {countriesToShow.map(country => (
+            <li key={country.name.common}>
+              {country.name.common}
+              <button onClick={() => onShowCountry(country)}>Show</button>
+            </li>
+          ))}
+        </ul>
+      )
+    }
+    else if (countriesToShow.length > 10) {
+      return <p>too many matches, specify another filter</p>
+    }
 }
 
 const App = () => {
@@ -43,9 +84,11 @@ const App = () => {
   const [newCountry, setNewCountry] = useState("")
   const [countries, setCountries] = useState([])
   const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all"
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   const handleChange = (event) => {
     setNewCountry(event.target.value)
+    setSelectedCountry(null)
   }
 
   useEffect(() => {
@@ -66,12 +109,11 @@ const App = () => {
         <p>find countries</p>
         <input value={newCountry} onChange={handleChange} />
       </form>
-      {/* <ul>
-        {countriesToShow.map(country => (
-          <li key={country.name.common}>{country.name.common}</li>
-        ))}
-      </ul> */}
-      <Display countriesToShow={countriesToShow} />
+      <Display
+        countriesToShow={countriesToShow}
+        selectedCountry={selectedCountry}
+        onShowCountry={setSelectedCountry}
+      />
     </div>
   )
 }
