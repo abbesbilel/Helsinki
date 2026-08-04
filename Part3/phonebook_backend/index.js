5c711de3868f2ebfd0757428d95f0dbc15151d39
abbesbilel <b_abbes@estin.dz>
2026-08-04 23:33:05 +0100

    uibuild

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -1,47 +1,28 @@
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.use(express.json())
 
 morgan.token('body', (request, response) => {
-    if(request.method === 'POST') {
+    if (request.method === 'POST') {
         return JSON.stringify(request.body)
     }
     return ''
 })
 
 app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -54,13 +35,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        response.send(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -71,29 +48,22 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons/', (request, response) => {
     const body = request.body
-    const existingName = persons.find(p => p.name === body.name)
-    if(!body.name || !body.number) {
+    if (!body.name || !body.number) {
         return response.status(400).json({
             error: "content missing"
         })
     }
-    if(existingName) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: String(Math.floor(Math.random() * 1000) + 1)
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`)
 })
