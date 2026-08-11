5ceda12e7dd49723c7b7c7dfacaa2e9d887fb281
abbesbilel <b_abbes@estin.dz>
2026-08-11 22:03:04 +0100

    finished exercises 18-19 part4

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,12 +19,13 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  let user = await User.findById(body.user)
 
-  if (!user) {
-    const users = await User.find({})
-    user = users[0]
+  const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
+
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'no user exists in database' })
--- Part4/bloglist/utils/config.js
+++ Part4/bloglist/utils/config.js
@@ -4,5 +4,6 @@ const mongoUrl = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.mongoUrl
 const PORT = process.env.PORT
+const SECRET = process.env.SECRET
 
-module.exports = { mongoUrl, PORT}
+module.exports = { mongoUrl, PORT, SECRET}
