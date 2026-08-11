95002ff6714af2108a43eff8d0fcdb22cf42c608
abbesbilel <b_abbes@estin.dz>
2026-08-11 19:33:12 +0100

    finished exercise 15 part4

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -22,6 +23,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
